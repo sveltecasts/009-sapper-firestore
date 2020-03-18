6c1b3d1e8820fe39403c20efac970422c6e8d7c3
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2020-03-18 15:56:56 -0700

    Firebase config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,11 @@
-export const firebaseConfig = {
-    apiKey: "",
-    authDomain: ".firebaseapp.com",
-    databaseURL: "https://.firebaseio.com",
-    projectId: "",
-    storageBucket: ".appspot.com",
-    messagingSenderId: "",
-    appId: ""
-};
+export const firebaseConfig = 
+    {
+        apiKey: "AIzaSyClTM0ddPhZgPoVtifd8TZ0c-LrC0iZshs",
+        authDomain: "covid19saratoga.firebaseapp.com",
+        databaseURL: "https://covid19saratoga.firebaseio.com",
+        projectId: "covid19saratoga",
+        storageBucket: "covid19saratoga.appspot.com",
+        messagingSenderId: "1092626000161",
+        appId: "1:1092626000161:web:86ad46738af11ef21cb660",
+        measurementId: "G-ZFG19C7YDN"
+      };
